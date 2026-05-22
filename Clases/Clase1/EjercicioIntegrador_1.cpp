9abc33241ef825784ac491202b79281a20d09bd5
Seb0Code <sebastiangutierrezc7@gmail.com>
2026-05-21 22:27:07 -0400

    feat(EjercicioIntegrador_1.cpp): Añadir nuevas funciones y variables -Añadir la funcion de limpiarPantalla mediante codigo ASCI -Añadir la funcion ingresar tamaño para ingresar tamaños correctos -Añadir la funcion llenarArrayDinamico -Añadir las variables de opcion y num -Añadir la estructura principal del menu -Definir la opcion de crear y llenar Array

--- Clases/Clase1/EjercicioIntegrador_1.cpp
+++ Clases/Clase1/EjercicioIntegrador_1.cpp
@@ -61,18 +61,40 @@ using std::string;
 
 void esperarSegundos() { std::this_thread::sleep_for(std::chrono::milliseconds(2500)); }
 
+void limpiarPantalla() {
+    cout << "\033[2J\033[1;1H" << std::flush; // Secuencia de escape ANSI para limpiar pantalla
+}
+
 void ingresarDatos(int &num) {
     bool flag;
     do {
         flag = false;
+        cout << "Ingresa Aqui: ";
         cin >> num;
         if (cin.fail()) {
             cin.clear();
             cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
             flag = true;
-            cout << "ERROR tipo de dato incorrecto";
+            cout << "\nERROR tipo de dato incorrecto";
         }
     } while (flag);
+}
+
+void ingresarTamanio(int &tamanio) {
+    bool flagTamanio;
+    // Bucle que ejecuta la accion de ingresar el tamaño hasta que el usuario ingrese un tamaño correcto
+    do {
+        // inicializamos la variable en false a cada vuelta de bucle
+        flagTamanio = false;
+        cout << "-----TAMANIO DEL ARRAY DINAMICO-------\n";
+        ingresarDatos(tamanio);
+
+        // si ingresa un tamaño incorrecto se activa la bandera
+        if (tamanio <= 0) {
+            flagTamanio = true;
+            cout << "ERROR\nIngrese un numero positivo";
+        }
+    } while (flagTamanio);
 }
 
 int *crearArrayDinamico(int tamanio) {
@@ -89,29 +111,70 @@ int *crearArrayDinamico(int tamanio) {
     return nuevoArray;
 }
 
+// Pasamos como parametros el tamaño y la direccion al primer elemento del array dinamico
+void llenarArrayDinamico(int tamanio, int *ptr) {
+    int num;
+    cout << "-------LLENAR ARRAY DINAMICO-------\n\n";
+
+    // bucle que recorre todo el array
+    for (size_t e = 0; e < tamanio; e++) {
+        ingresarDatos(num);
+        *(ptr + e) = num;
+    }
+}
+
 int main() {
     // variables
     int tamanio;
     bool flagTamanio = false;
+    int opcion = 0;
+    int num = 0;
 
     // punteros
     int *ArrayPtr;
 
     //* Comienzo del programa
 
-    // Bucle que ejecuta la accion de ingresar el tamaño hasta que el usuario ingrese un tamaño correcto
     do {
-        // inicializamos la variable en false a cada vuelta de bucle
-        flagTamanio = false;
-        cout << "Ingresa el tamaño del array dinamico: ";
-        ingresarDatos(tamanio);
+        cout << "\n\n-------MENU-------\n\n";
+        cout << "1. Crear y llenar arreglo\n2. Mostrar arreglo\n";
+        cout << "3. Encontrar número mayor\n4. Calcular promedio\n5. Salir";
+        ingresarDatos(opcion);
 
-        // si ingresa un tamaño incorrecto se activa la bandera
-        if (tamanio <= 0) {
-            flagTamanio = true;
-            cout << "ERROR\nIngrese un numero positivo";
+        switch (opcion) {
+            case 1:
+                limpiarPantalla();
+                cout << "\n\n-------CREAR ARRAY DINAMICO-------\n\n";
+                ingresarTamanio(tamanio);
+                crearArrayDinamico(tamanio);
+                cout << "Array creado con exito\n";
+                limpiarPantalla();
+                llenarArrayDinamico(tamanio, ArrayPtr);
+                cout << "Array llenado con exito";
+                break;
+
+            case 2:
+                limpiarPantalla();
+                break;
+
+            case 3:
+                limpiarPantalla();
+                break;
+
+            case 4:
+                limpiarPantalla();
+                break;
+
+            case 5:
+                limpiarPantalla();
+                break;
+
+            default:
+                limpiarPantalla();
         }
-    } while (flagTamanio);
+    } while (opcion != 5);
+
+
 
     // Guardamos la direccion del arrayDinamico en nuestra variable dentro del main
     ArrayPtr = crearArrayDinamico(tamanio);
